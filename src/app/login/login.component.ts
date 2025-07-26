e994ac9f928eed310eda192e834f1bfb5d0e9900
DevoScientist <anubhab13rta@gmail.com>
2025-07-26 17:10:48 +0530

    set and set in Input tag learned @login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  name = "";
+  displayName="";
+  email="";
+
+  getName(event:Event){
+    const val = (event.target as HTMLInputElement).value;
+    this.name = val;
+  }
+
+  showName(event:Event){
+    this.displayName=this.name;
+    return this.displayName;
+  }
+
+  setName(){
+    this.name= "default@459"
+  }
+
+  getEmail(val:string){
+    this.email = val;
+    console.log(this.email);
+  }
+
+  setEmail(){
+    this.email ='default@example.com';
+  }
 }
