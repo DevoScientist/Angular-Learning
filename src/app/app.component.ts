2399863e328af10352334d2472034f09e2396ff3
DevoScientist <anubhab13rta@gmail.com>
2025-07-27 00:33:44 +0530

    Dynamic styling, Directives completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CounterComponent } from "./counter/counter.component";
 import { FormsModule } from '@angular/forms'; //Ussed for Two-way-binding
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, CounterComponent, FormsModule],
+  imports: [LoginComponent, CounterComponent, FormsModule, NgIf, NgFor,
+    NgSwitch,NgSwitchCase, NgSwitchDefault
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,11 +19,92 @@ import { FormsModule } from '@angular/forms'; //Ussed for Two-way-binding
 
 export class AppComponent {
 
-  task=""
-  private currentId = 1
+  //---------------NgSwitch------------------------------------
 
-  taskList:{id:number,taskName:string}[]=[]
-  addTask(){
+  colour = ''
+  changeColor(colour:string){
+    this.colour=colour
+  }
+
+  //---------------NgIf------------------------------------
+
+  // block = 0
+  // onClick(){
+  //   this.block++;
+  // }
+
+  // login = true
+
+  // authControll(){
+  //   this.login=!this.login
+  // }
+
+
+  //------------- NgFor---------------------------------------------------
+
+//   students = ["Anil","Sam", "Peter","Vinay", "Bruce"];
+
+//   studentData = [
+//   {
+//     name: "John Doe",
+//     age: 28,
+//     email: "john.doe@example.com"
+//   },
+//   {
+//     name: "Jane Smith",
+//     age: 34,
+//     email: "jane.smith@example.com"
+//   },
+//   {
+//     name: "Sam Brown",
+//     age: 22,
+//     email: "sam.brown@example.com"
+//   },
+//   {
+//     name: "Lisa White",
+//     age: 40,
+//     email: "lisa.white@example.com"
+//   },
+//   {
+//     name: "Mike Green",
+//     age: 30,
+//     email: "mike.green@example.com"
+//   }
+// ]
+
+
+  //-----------Directives--------------------------------------------------
+
+  // show = false
+
+  // showToggle(){
+  //   this.show = !this.show
+  // }
+
+
+
+  // ----------Dynamic Styling------------------------------------------------
+
+  // bgColor="green"
+  // fontSize="30"
+  // headingSizeBig="80px"
+  // headingSizeSmall="30px"
+
+  // zoom=true
+  // updateHeadingSize() {
+  //   this.zoom=!this.zoom
+  //   console.log("Anubhab === <3 Angular");
+    
+  // }
+
+
+  // --------------------------------------------------------------
+
+  // task=""
+  // private currentId = 1
+
+  // taskList:{id:number,taskName:string}[]=[]
+  // addTask(){
     // const newTask = {
     //   id : this.currentId,
     //   taskName : this.task
@@ -31,13 +116,13 @@ export class AppComponent {
 
     //___________ method 2_________________________________
 
-    this.taskList.push({id:this.taskList.length+1 , taskName:this.task})
-    this.task=''
-  }
+  //   this.taskList.push({id:this.taskList.length+1 , taskName:this.task})
+  //   this.task=''
+  // }
 
-  deleteTask(taskId:number){
-    this.taskList = this.taskList.filter((TASK )=> TASK.id!= taskId)
-  }
+  // deleteTask(taskId:number){
+  //   this.taskList = this.taskList.filter((TASK )=> TASK.id!= taskId)
+  // }
 
 
 // ----------------------------------------------------------------------------------------
