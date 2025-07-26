ba5c9d0551c8995ce1d6084879597678083b8913
DevoScientist <anubhab13rta@gmail.com>
2025-07-26 15:40:23 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CounterComponent } from "./counter/counter.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tutorial';
+  handleClickEvent() {
+    alert('Button clicked!');
+
+    this.otherFunction();
+  }
+
+  otherFunction() {
+    alert('This is another function in the AppComponent.');
+  }
 }
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  count:number = 0;
+
+  handleCount(val:string){
+    if(val == 'minus' && this.count>0){
+      this.count--;
+    }
+    if(val == 'plus'){
+      this.count++;
+    }
+    if(val == 'reset'){
+      this.count=0;
+    }
+
+  }
+}
